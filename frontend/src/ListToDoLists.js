baa38f7d27b3644d85197696eb5c47b8bf4fa713
arlankarlos <lolozo@msn.com>
2024-09-26 18:35:01 -0300

    First commit. Corrigir bugs

---
+++ frontend/src/ListToDoLists.js
@@ -0,0 +1,61 @@
+import "./ListToDoLists.css";
+import {useRef} from "react";
+import {BiSolidTrash} from "react-icons/bi";
+
+function ListToDoLists({
+    listSummaries,
+    handleNewToDoList,
+    handleDeleteToDoList,
+    handleSelectList,
+}) {
+    const labelRef = useRef();
+
+    if (listSummaries === null) {
+        return <div className="ListToDoLists loading">Loading... to-do lists...</div>;
+} else if (listSummaries.length === 0) {
+        return (<div className="ListToDoLists">
+            <div className="box">
+                <label htmlFor="new-to-do-list">New To-Do List: 
+                    <input id={labelRef}type="text" />
+                    </label>
+                    <button onClick={() => 
+                    handleNewToDoList(document.getElementById(labelRef).value)}>New</button>
+                    
+            </div>
+            <p>There are no to-do lists!</p>
+        </div>);
+    }
+    return (
+        <div className="ListToDoLists">
+            <h1>All To-Do Lists</h1>
+            <div className="box">
+                <label htmlFor="new-to-do-list">New To-Do List:&nbsp; 
+                    <input id={labelRef}type="text" />
+                    </label>
+                    <button onClick={() => 
+                    handleNewToDoList(document.getElementById(labelRef).value)}>
+                        New</button>
+            </div>
+            {listSummaries.map((summary) =>{
+                return(
+                <div
+                    className="summary"
+                    key={summary.id}
+                    onClick={() => handleSelectList(summary.id)}
+                >
+                    <span className="name">{summary.name}</span>
+                    <span className="count">{summary.item_count} items</span>
+                    <span className="flex"></span>
+                    <span className="trase"
+                    onClick={(evt) =>{
+                        evt.stopPropagation();
+                        handleDeleteToDoList(summary.id);
+                    }}><BiSolidTrash/>
+                    </span>
+                </div>
+            );
+            })}
+        </div>
+        );
+}
+export default ListToDoLists;
